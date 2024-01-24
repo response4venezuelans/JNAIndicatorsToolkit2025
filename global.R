b797d66e8c3c013ebf24174dfaf4521a5a3feeff
Jorge Cabrera <cabrerjo@unhcr.org>
2024-01-24 11:33:51 -0500

    Fix start dataframe

--- global.R
+++ global.R
@@ -14,11 +14,19 @@ library(writexl)
 ## -- common functions and global variables --
 source("functions/utils.R")
 appColors <- c("#00AAAD", "#007681", "#004F66", "#132A3E")
-path <- "C:/Users/CABRERJO/International Organization for Migration - IOM/Regional Cordination Platform - General/IM/JNAs & Sector Guides/PiN Indicators 2024/Compilado de fichas (post-taller Dec 23)/Respuesta final de sectores"
-SourceJNA2024Indicators <- read_excel(
-  paste(path, "Compilado de fichas sectores_FINAL.xlsx", sep="/"), 
-  sheet = "DB_MERGED") %>% 
-  select(1:15)
+
+# path <- "C:/Users/CABRERJO/International Organization for Migration - IOM/Regional Cordination Platform - General/IM/JNAs & Sector Guides/PiN Indicators 2024/Compilado de fichas (post-taller Dec 23)/Respuesta final de sectores"
+# SourceJNA2024Indicators <- read_excel(
+#   paste(path, "Compilado de fichas sectores_FINAL.xlsx", sep="/"), 
+#   sheet = "DB_MERGED") %>% 
+#   select(1:15)
+# 
+# # Save dataframe to RDS
+# saveRDS(SourceJNA2024Indicators, "datasource/JNAToolkitDF.RDS")
+
+# Read dataframe from RDS
+read_dataframe_rds <- readRDS("datasource/JNAToolkitDF.RDS")
+
 JNA2024Indicators <- SourceJNA2024Indicators
 
 ##-- Calling componets for shiny header  ----

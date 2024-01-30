dd767417ca8f66ce0f13160ca9ab66d7dbbd1a61
Jorge Cabrera <cabrerjo@unhcr.org>
2024-01-30 15:48:13 -0500

    commented read excel file

--- global.R
+++ global.R
@@ -16,11 +16,11 @@ setwd(here())
 source("functions/utils.R")
 appColors <- c("#00AAAD", "#007681", "#004F66", "#132A3E")
 
-path <- "C:/Users/CABRERJO/International Organization for Migration - IOM/Regional Cordination Platform - General/IM/JNAs & Sector Guides/PiN Indicators 2024/Compilado de fichas (post-taller Dec 23)/Respuesta final de sectores"
-SourceJNA2024Indicators <- read_excel(
-  paste(path, "0_Compilado_fichas_JNA.xlsx", sep="/"),
-  sheet = "DB_MERGED_Final") %>%
-  select(1:15)
+# path <- "C:/Users/CABRERJO/International Organization for Migration - IOM/Regional Cordination Platform - General/IM/JNAs & Sector Guides/PiN Indicators 2024/Compilado de fichas (post-taller Dec 23)/Respuesta final de sectores"
+# SourceJNA2024Indicators <- read_excel(
+#   paste(path, "0_Compilado_fichas_JNA.xlsx", sep="/"),
+#   sheet = "DB_MERGED_Final") %>%
+#   select(1:15)
 
 # # Save dataframe to txt
 # dput_output <- capture.output(dput(SourceJNA2024Indicators))

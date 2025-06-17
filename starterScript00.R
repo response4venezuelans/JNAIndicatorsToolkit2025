19f930b994f340a94326a02d12c603019869587d
Jorge Cabrera <cabrerjo@unhcr.org>
2025-06-17 09:39:44 -0500

    2025

--- starterScript00.R
+++ starterScript00.R
@@ -2,11 +2,11 @@
 library(readxl)
 library(dplyr)
 ## -- read indicator table --
-path <- "C:/Users/CABRERJO/International Organization for Migration - IOM/Regional Cordination Platform - General/IM/JNAs & Sector Guides/PiN Indicators 2024/Compilado de fichas (post-taller Dec 23)/Respuesta final de sectores"
+path <- "C:/personal_dev/JNAShinyApp2025/dataSource"
 SourceJNA2024Indicators <- read_excel(
-  paste(path, "0_Compilado_fichas_JNA.xlsx", sep="/"),
-  sheet = "DB_MERGED_Final") %>%
-  select(1:15)
+  paste(path, "Consolidated Framework PowerQuery2025.xlsx", sep="/"),
+  sheet = "All") %>%
+  select(1:17)
 
 # Save dataframe to txt
 dput_output <- capture.output(dput(SourceJNA2024Indicators))
@@ -14,7 +14,3 @@ writeLines(dput_output, "datasource/JNAToolkit.txt")
 
 
 #saveRDS(SourceJNA2024Indicators, "datasource/JNAToolkitDF.RDS")
-# 
-# # Read dataframe from RDS
-# #read_dataframe_rds <- readRDS("datasource/JNAToolkitDF.RDS")
-# SourceJNA2024Indicators <- source("datasource/JNAToolkit.txt")
